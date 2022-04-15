155de84586e147174a2236c1e534ab230c1dda1e
crochetcode <fred.davison@outlook.com>
2022-04-15 08:30:03 -0500

    WIP

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,6 +2,7 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import static com.gildedrose.GildedRose.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
@@ -40,24 +41,51 @@ class GildedRoseTest {
 
     @Test
     public void agedBrieIncreasesInQuality() {
-        Item item = createAndUpdate("Aged Brie", 15, 25);
+        Item item = createAndUpdate(AGED_BRIE, 15, 25);
         assertEquals(26, item.quality);
     }
 
     @Test
     public void agedBrieNeverExpires() {
-        Item item = createAndUpdate("Aged Brie", 0, 42);
+        Item item = createAndUpdate(AGED_BRIE, 0, 42);
         assertEquals(-1, item.sellIn);
         assertEquals(44, item.quality);
+
+        item = createAndUpdate(AGED_BRIE, 0, MAXIMUM_QUALITY);
+        assertEquals(MAXIMUM_QUALITY, item.quality);
     }
 
     @Test
     public void backstagePassMaximumQuality() {
-        Item item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 10, 48);
-        assertEquals(50, item.quality);
+        Item item = createAndUpdate(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 10, 48);
+        assertEquals(MAXIMUM_QUALITY, item.quality);
 
-        item = createAndUpdate("Backstage passes to a TAFKAL80ETC concert", 10, 49);
-        assertEquals(50, item.quality);
+        item = createAndUpdate(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 10, 49);
+        assertEquals(MAXIMUM_QUALITY, item.quality);
+
+        item = createAndUpdate(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 5, 24);
+        assertEquals(27, item.quality);
+    }
+
+    @Test
+    public void whenSellInIsLessThanZero() {
+        Item item = createAndUpdate("foo", 0, 2);
+        assertEquals(0, item.quality);
+    }
+
+    @Test
+    public void sulfurasHandOfRagnaros() {
+        Item item = createAndUpdate(SULFURAS_HAND_OF_RAGNAROS, 0, 2);
+        assertEquals(2, item.quality);
+    }
+
+    @Test
+    public void backStagePassesSellInLessThanZero() {
+        Item item = createAndUpdate(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 0, 24);
+        assertEquals(0, item.quality);
+
+        item = createAndUpdate(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 0, 0);
+        assertEquals(0, item.quality);
     }
 
 }
